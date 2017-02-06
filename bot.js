f90f72cdda5309a9aa97c261be700ec74162a884
Mellanfil <Mellanfil@users.noreply.github.com>
2017-02-06 21:23:42 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,35 @@
+var TelegramBot = require('node-telegram-bot-api');
+var token = '292344842:AAF-xVwiz_cO11o1_VgnqHbOvpFwd3VKOVo';
+var emoji = require("node-emoji").emoji;
+ 
+// Create a bot that uses 'polling' to fetch new updates 
+var bot = new TelegramBot(token, { polling: true });
+
+bot.getUpdates()
+ 
+bot.onText(/\/nybajs/, function(msg, match) {
+    const photo = "./poop_emoji.png";
+    var chatId = msg.chat.id;
+    var option = {
+            parse_mode: "Markdown",
+            reply_markup: {  
+                resize_keyboard: true,
+                keyboard: [["Svart"],["Brun"],["Grön"]],
+                one_time_keyboard: true
+            }
+            
+        };
+
+    var nybajs = ["Ah det va fan på tiden...", "Har du inte förstört toaletten tillräckligt?", "Ouf, stackars toalett"];
+    var chosenBajs = nybajs[Math.floor(Math.random() * nybajs.length)];
+    bot.sendMessage(chatId, emoji.poop + chosenBajs + emoji.poop)
+    bot.sendMessage(chatId, "Färg?", option);
+    
+});
+
+bot.onText(/\/help/, function(msg, match) {
+    var chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Jag har bara ett kommando.\n/nybajs - Lägg till en ny bajsupplevelse")
+});
+
+console.log("PooBot is running");
